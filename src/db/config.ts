86e2ea6dedb03e16c3acdf4f386bf9627c399211
Aniket <aniketraj28042003@gmail.com>
2024-09-27 00:32:34 +0530

    fix : added fixes for db connection

--- src/db/config.ts
+++
@@ -1,7 +0,0 @@
-export const options = {
-    HOST: process.env.DB_HOST as string,
-    USER: process.env.DB_USER as string,
-    PASSWORD: process.env.DB_PASSWORD as string,
-    DB: process.env.DB_DB as string,
-    DIALECT: "mysql"
-};
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,28 +1,8 @@
-import { Sequelize } from "sequelize";
+import Sequelize from "sequelize";
+import config from "./config/config";
 
-import { options } from "./config";
+const sequelize = new Sequelize(config.development);
 
-const sequelize = new Sequelize({
-    host: options.HOST,
-    username: options.USER,
-    password: options.PASSWORD,
-    database: options.DB,
-    dialect: options.DIALECT,
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    dialectModule: require("mysql2"),
-    benchmark: true
-});
+const connection = sequelize;
 
-const dbConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("DB connected successfully");
-        await sequelize.sync({ alter: true });
-    } catch (error) {
-        console.log("Unable to connect DB with error...", error);
-    }
-};
-
-dbConnection();
-
-export default sequelize;
+export default connection;
